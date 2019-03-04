1c50c3edbe93b7d1c46a8f258895f1d5780b9d67
gsw945 <gogs@fake.local>
2019-03-04 14:04:14 +0800

    bug修复

--- pages/models.py
+++ pages/models.py
@@ -6,9 +6,9 @@ from django.utils import timezone
 class Article(models.Model):
     title = models.CharField('标题', max_length=200)
     content = models.TextField()
-    create_time = models.DateTimeField('创建时间', default=timezone.now())
+    create_time = models.DateTimeField('创建时间', default=timezone.now)
     upate_time = models.DateTimeField('最后修改时间', auto_now=True)
-    tags = models.ManyToManyField('Tag', through='ArticleTags', related_name='articles', blank=True)
+    tags = models.ManyToManyField('Tag', through='ArticleTags')
 
     def __str__(self):
         return self.title
