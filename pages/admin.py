260769c5f8ebee63b6e32463e918d2317d3d65c5
gsw945 <gogs@fake.local>
2019-03-05 09:47:21 +0800

    添加模型到Admin中

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article, Tag, ArticleTags
+
+
+class TagsInlineAdmin(admin.StackedInline):
+    model = Article.tags.through
+    extra = 0 # 去掉空行
+    can_delete = True # 让文章关联的标签可以移除
+
+class ArticleAdmin(admin.ModelAdmin):
+    # save_on_top = True
+    fieldsets = [
+        (None, {'fields': ['title', 'content', 'create_time', 'upate_time']}),
+    ]
+    readonly_fields = ['create_time', 'upate_time'] # 只读字段
+    # show_change_link = True
+    inlines = (TagsInlineAdmin,)
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Tag)
+# admin.site.register(ArticleTags)
--- pages/models.py
+++ pages/models.py
@@ -5,13 +5,18 @@ from django.utils import timezone
 
 class Article(models.Model):
     title = models.CharField('标题', max_length=200)
-    content = models.TextField()
+    content = models.TextField(verbose_name='内容')
     create_time = models.DateTimeField('创建时间', default=timezone.now)
     upate_time = models.DateTimeField('最后修改时间', auto_now=True)
     tags = models.ManyToManyField('Tag', through='ArticleTags')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
+        app_label = 'pages'
 
 class Tag(models.Model):
     name = models.CharField('标签名', max_length=30)
@@ -20,9 +25,19 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = '标签'
+        app_label = 'pages'
+
 class ArticleTags(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
     def __str__(self):
         return '{0}[{1}]'.format(self.article.title, self.tag.name)
+
+    class Meta:
+        verbose_name = '文章标签'
+        verbose_name_plural = '文章标签'
+        app_label = 'pages'
