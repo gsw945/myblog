a36ffd5edc207fece86a440d67e615396b91e9cb
gsw945 <gsw945@foxmail.com>
2019-03-21 21:14:58 +0800

    修改博文访问统计接口频率限制

--- pages/apis.py
+++ pages/apis.py
@@ -7,17 +7,34 @@ from .models import ArticleAnalysis, Article
 
 
 def ratelimit_key(group, request):
-    client_ip, is_routable = get_client_ip(request)
-    req_article = getattr(request, request.method.upper(), request.POST).get('article', None)
-    if not (bool(req_article) and req_article.isdigit()):
-        req_article = None
-    setattr(request, 'req_article', req_article)
-    setattr(request, 'client_ip', client_ip)
-    setattr(request, 'is_routable', is_routable)
+    req_article = getattr(request, 'req_article', None)
+    if bool(req_article):
+        client_ip = getattr(request, 'client_ip', None)
+        is_routable = getattr(request, 'is_routable', None)
+    else:
+        # 获取访客端IP
+        client_ip, is_routable = get_client_ip(request)
+        # 获取请求参数中的博文ID
+        req_article = getattr(request, request.method.upper(), request.POST).get('article', None)
+        # 判断博文ID有效性
+        if not (bool(req_article) and req_article.isdigit()):
+            req_article = None
+        # 附加属性到request，让后续的视图函数中可以使用
+        setattr(request, 'req_article', req_article)
+        setattr(request, 'client_ip', client_ip)
+        setattr(request, 'is_routable', is_routable)
     return '{0}-{1}-{2}'.format(client_ip, group, req_article)
 
-@ratelimit(key=ratelimit_key, rate='1/10s', method=ratelimit.ALL, block=False)
+@ratelimit(key=ratelimit_key, rate='60/d', method=ratelimit.ALL, block=False)
+@ratelimit(key=ratelimit_key, rate='12/h', method=ratelimit.ALL, block=False)
+@ratelimit(key=ratelimit_key, rate='1/12s', method=ratelimit.ALL, block=False)
 def api_article_analysis(request):
+    '''
+    60/d: 每天60次限制
+    12/h: 每小时12次限制
+    1/12s: 每12秒一次限制
+    '''
+    # 是否被限制
     is_limited = getattr(request, 'limited', False)
     if is_limited:
         ret = {
@@ -25,13 +42,16 @@ def api_article_analysis(request):
             'desc': '访问过于频繁，请稍后再试'
         }
     else:
+        # 博文id
         req_article = getattr(request, 'req_article', None)
         if bool(req_article):
             article_id = int(req_article)
+            # 根据博文id查询博文对象
             article = Article.objects.filter(pk=article_id).first()
             if isinstance(article, Article):
                 client_ip = getattr(request, 'client_ip', None)
                 is_routable = getattr(request, 'is_routable', None)
+                # 新的博文访问记录
                 obj = ArticleAnalysis(article=article, client_ip=client_ip, is_routable=is_routable)
                 obj.save()
                 ret = {
